f6e461e4712962f7ed452e393cebfc6a943b2351
ujjwalk2523 <ujjwalk2523@gmail.com>
2026-02-09 16:41:33 +0530

    DSA Day 03 - loops-alphabets

---
+++ src/loops/alphabets.java
@@ -0,0 +1,10 @@
+package loops;
+import java.util.Scanner;
+public class alphabets {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        for(int i=1;i<=126;i++){
+            System.out.println((char)i+":"+ i);
+        }
+    }
+}
