8e38ead2466bc8bf2b2cf1eeb8a93d38cb4b895e
ujjwalk2523 <ujjwalk2523@gmail.com>
2026-02-09 16:42:30 +0530

    DSA Day 03 - loops-count digits

---
+++ src/loops/countdigits.java
@@ -0,0 +1,17 @@
+package loops;
+import java.util.Scanner;
+public class countdigits {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a number:");
+        int n=sc.nextInt();
+        if(n==0) n=1;
+        int count=0;
+        while(n!=0){
+
+            n=n/10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
