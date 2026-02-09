5e27059806364f8be842c5daa6367385fdd3ea04
ujjwalk2523 <ujjwalk2523@gmail.com>
2026-02-09 16:44:57 +0530

    DSA Day 03 - loops-print 1 to 10

---
+++ src/loops/print1to10.java
@@ -0,0 +1,10 @@
+package loops;
+import java.util.Scanner;
+public class print1to10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
