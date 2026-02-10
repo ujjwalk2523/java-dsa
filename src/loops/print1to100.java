00cd7368fc6baef26dd88e5d2c55493e34e5a1b2
ujjwalk2523 <ujjwalk2523@gmail.com>
2026-02-10 18:03:01 +0530

    DSA Day 04 - loops-print 1 to 100

---
+++ src/loops/print1to100.java
@@ -0,0 +1,10 @@
+package loops;
+import java.util.Scanner;
+public class print1to100 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int j=1;j<=100;j++){
+            System.out.print(j+" ");
+        }
+    }
+}
