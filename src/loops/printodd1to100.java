4d51aa980d84bf3f9e6bc94f64872c05a8c8179b
ujjwalk2523 <ujjwalk2523@gmail.com>
2026-02-10 18:03:59 +0530

    DSA Day 04 - loops-print odd 1 to 100

---
+++ src/loops/printodd1to100.java
@@ -0,0 +1,8 @@
+package loops;
+import java.util.Scanner;
+public class printodd1to100 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=1;i<=100;i++){if(i%2==1) System.out.print(i +" ");}
+    }
+}
