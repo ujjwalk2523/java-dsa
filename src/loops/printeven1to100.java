a5e9fe010bb01940ffd7c6c7f26c1070d7bcc22d
ujjwalk2523 <ujjwalk2523@gmail.com>
2026-02-10 18:03:28 +0530

    DSA Day 04 - loops-printeven 1 to 100

---
+++ src/loops/printeven1to100.java
@@ -0,0 +1,13 @@
+package loops;
+import java.util.Scanner;
+
+public class printeven1to100 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=1;i<=100;i++){if(i%2==0) System.out.println(i);
+
+
+        }
+    }
+}
