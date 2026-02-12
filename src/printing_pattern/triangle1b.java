8bf4028622917ec2ca0d3ef5e2fe75e9788fd1ba
ujjwalk2523 <ujjwalk2523@gmail.com>
2026-02-12 22:55:34 +0530

    DSA Day 06- printing-tr2

---
+++ src/printing_pattern/triangle1b.java
@@ -0,0 +1,15 @@
+package printing_pattern;
+import java.util.Scanner;
+public class triangle1b {
+    public static  void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter size:");
+        int n= sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
