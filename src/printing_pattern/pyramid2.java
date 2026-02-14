b909b4b1d50dbf26bed7c376314e47b33a2b5edf
ujjwalk2523 <ujjwalk2523@gmail.com>
2026-02-14 08:11:00 +0530

    DSA Day 07- printing-pyramid2

---
+++ src/printing_pattern/pyramid2.java
@@ -0,0 +1,18 @@
+package printing_pattern;
+import java.util.Scanner;
+public class pyramid2 {
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+        System.out.print("enter size:");
+            int n=sc.nextInt();
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n-i;j++){
+                    System.out.print("  ");
+                }
+                for(int j=1;j<=2*i-1;j++){
+                    System.out.print(i+" ");
+                }
+                System.out.println();
+            }
+    }
+}
