3e3116851c85fb52b12769ef778c1b71ec886b1d
ujjwalk2523 <ujjwalk2523@gmail.com>
2026-02-14 08:13:12 +0530

    DSA Day 07- printing-odd no triangle

---
+++ src/printing_pattern/oddnotriangle.java
@@ -0,0 +1,18 @@
+package printing_pattern;
+import java.util.Scanner;
+public class oddnotriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter size:");
+        int n=sc.nextInt();
+        int a=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(a+" ");
+                a+=2;
+            }
+            System.out.println();
+        }
+
+    }
+}
