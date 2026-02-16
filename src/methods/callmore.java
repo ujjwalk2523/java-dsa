f182cd8de607ebb90e8f8f09820d3a792a9467f2
ujjwalk2523 <ujjwalk2523@gmail.com>
2026-02-16 22:34:48 +0530

    DSA Day 09- methods call more

---
+++ src/methods/callmore.java
@@ -0,0 +1,31 @@
+package methods;
+import java.util.Scanner;
+public class callmore {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        shivani();
+        int total=me();
+
+        System.out.println("BC:"+total);
+    }
+    public static void shivani(){
+        System.out.println("HELLO UJJWAL");
+        pritika();
+    }
+    public static void pritika(){
+        System.out.println("hello bby");
+        kajal();
+    }
+    public static void kajal(){
+        System.out.println("love uhh");
+        arya();
+    }
+    public static void arya(){
+        System.out.println("miss uhh");
+        me();
+    }
+    public static int me(){
+
+        return 6;
+    }
+}
