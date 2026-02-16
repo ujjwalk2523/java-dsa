cf13163035e20b675726d33b393a3149d1f82bb7
ujjwalk2523 <ujjwalk2523@gmail.com>
2026-02-16 22:36:56 +0530

    DSA Day 09- methods sum

---
+++ src/methods/sum.java
@@ -0,0 +1,11 @@
+package methods;
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args) {
+        System.out.println(total(77,75,82));
+    }
+    public static int total(int a, int b, int c){
+        return a+b+c;
+    }
+
+}
