c1e05d4c917ce7ace8113c38bfdc15de213da7c4
ujjwalk2523 <ujjwalk2523@gmail.com>
2026-03-20 14:57:17 +0530

    DSA Day 28 cyclicsort-missinginArray1ton

--- src/cyclicsort/missinginArray1ton.java
+++ src/cyclicsort/missinginArray1ton.java
@@ -1,6 +1,6 @@
 package cyclicsort;
 import java.util.Scanner;
-public class missinginarray1ton {
+public class missinginArray1ton {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the size of the array:");
