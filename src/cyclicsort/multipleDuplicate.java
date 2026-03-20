f84f29f75184932c5eb97eba7ba0099f43c6fba8
ujjwalk2523 <ujjwalk2523@gmail.com>
2026-03-20 15:01:16 +0530

    DSA Day 28 cyclicsort-multipleDuplicate

--- src/cyclicsort/multipleDuplicate.java
+++ src/cyclicsort/multipleDuplicate.java
@@ -1,7 +1,7 @@
 package cyclicsort;
 import java.util.ArrayList;
 import java.util.Scanner;
-public class multipleduplicate {
+public class multipleDuplicate {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the size of array:");
